d0513409f51d0eb26d214792b2e469816db96e71
CTFang Windows <andy89923@gmail.com>
2023-03-12 17:23:36 +0800

    Week04

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # IoTtalk
 【111 Spring】515610物聯網應用設計與實作 Design and Implementation of IOT Applications
+
+Conda Environment:
+
+```shell
+conda activate iottalk
+```
+
---
+++ Week04/Part2/D3.py
@@ -0,0 +1,88 @@
+import time, random 
+import requests
+import threading, sys
+import DAN
+
+ServerURL = 'https://3.iottalk.tw/'
+
+Reg_addr = None
+
+mac_addr = 'CD8600D38' + '3333'
+
+Reg_addr = mac_addr
+DAN.profile['dm_name'] = 'Dummy_Device'
+
+DAN.profile['df_list'] = ['Dummy_Sensor', 'Dummy_Control']   # Check IoTtalk to see what IDF/ODF the DM has
+
+DAN.profile['d_name']  = "CTF.3" + "_" + DAN.profile['dm_name']
+
+DAN.device_registration_with_retry(ServerURL, Reg_addr) 
+
+print("dm_name is ", DAN.profile['dm_name']) ; print("Server is ", ServerURL);
+
+gotInput = False
+theInput = "haha"
+allDead = False
+
+def doRead( ):
+    global gotInput, theInput, allDead
+    while True:   
+        while gotInput:   # 老闆還沒把資料拿走
+           time.sleep(0.1)    # 小睡 下把 CPU 暫時讓給別人
+           continue  # go back to while   
+        try:     # 準備讀取資料, 注意程式會卡在這等 User 輸入, 所以要用 Thread
+           theInput = input("Give me data: ")
+        except Exception:    ##  KeyboardInterrupt:
+           allDead = True
+           print("\n\nDeregister " + DAN.profile['d_name'] + " !!!\n",  flush=True)
+           DAN.deregister()
+           sys.stdout = sys.__stdout__
+           print(" Thread say Bye bye ---------------", flush=True)
+           sys.exit( );   ## break  # raise   #  ?
+        if theInput =='quit' or theInput == "exit":    # these are NOT data
+           allDead = True
+        else:
+           print("Will send " + theInput, end="   , ")
+           gotInput=True   # notify my master that we have data 
+        if allDead: break;   # 離開 while True 這 Loop  
+
+#creat a thread to do Input data from keyboard, by tsaiwn@cs.nctu.edu.tw 
+threadx = threading.Thread(target=doRead)
+threadx.daemon = True  # 這樣才不會阻礙到主程式的結束
+threadx.start()
+
+while True:
+    try:
+        if(allDead): break;
+    #Pull data from a device feature called "Dummy_Control"
+        value1=DAN.pull('Dummy_Control')
+        if value1 != None:    # 不等於 None 表示有抓到資料
+            print (value1[0])
+        if gotInput:
+           try:
+              value2=float( theInput )   # 故意轉成實數 
+           except:
+              value2=0   # 轉成實數失敗就當作 0.0 
+           if(allDead): break;
+           gotInput=False   # so that you can input again  # 讓小弟知道我拿走了  
+           DAN.push ('Dummy_Sensor', value2,  value2)  #  試這:  DAN.push('Dummy_Sensor', theInput) 
+
+    except Exception as e:
+        print(e)
+        if str(e).find('mac_addr not found:') != -1:
+            print('Reg_addr is not found. Try to re-register...')
+            DAN.device_registration_with_retry(ServerURL, Reg_addr)
+        else:
+            print('Connection failed due to unknow reasons.')
+            time.sleep(1)    
+    try:
+       time.sleep(0.2)
+    except KeyboardInterrupt:
+       break
+time.sleep(0.25)
+try: 
+   DAN.deregister()    # 試著解除註冊
+except Exception as e:
+   print("===")
+print("Bye ! --------------", flush=True)
+sys.exit( );
